5795323e76bf072e9036ec829de123d8fd5b6bf8
Hossein Moradi <hossein.moradi.sarvandi@gmail.com>
2025-12-22 21:04:03 +0100

    feat: exploit coefficient symmetry to reduce FIR logic usage

--- Source/fir.cpp
+++ Source/fir.cpp
@@ -8,20 +8,29 @@ void fir (
 {
 	static data_t shift_reg[N];
 	acc_t acc,temp;
-	data_t data;
+	data_t data, dat2;
 	int i;
 	acc=0;
 	Shift_Accum_Loop:
 	for (i=N-1;i>=0;i--) {
 		if (i==0) {
 			shift_reg[0]=x;
-			data = x;
 		}
 		else {
 			shift_reg[i]=shift_reg[i-1];
-			data = shift_reg[i];
 		}
-		temp = data*c[i];
+	}
+	Mirroring_Accum_Loop:
+	for (i=N/2-1;i>=0;i--) {
+		if (i==0) {
+			data = x;
+			dat2 = shift_reg[N-1];
+		}
+		else {
+			data = shift_reg[i];
+			dat2 = shift_reg[N-i-1];
+		}
+		temp = (data + dat2)*c[i];
 		acc+=temp;
 	}
 	Shifting_Part:
