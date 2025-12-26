3fa2b97816580c6babb8a460ebf1fa1aa6438782
Hossein Moradi <hossein.moradi.sarvandi@gmail.com>
2025-12-26 14:50:24 +0100

    feat: use array partition to achieve II of one

--- run/directives1.tcl
+++ run/directives1.tcl
@@ -7,3 +7,5 @@ set_directive_pipeline -II 1 "fir"
 set_directive_interface -mode ap_vld "fir" y
 set_directive_bind_storage -type rom_1p -impl bram "fir" c
 set_directive_interface -mode ap_vld "fir" x
+set_directive_array_partition -dim 1 -type complete "fir" c
+set_directive_array_partition -dim 1 -type complete "fir" shift_reg
